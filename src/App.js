fcbe09074c92ff767682485d4fd3aed04b612355
“[firstname <aliimran98887@gmail.com>
2024-01-05 23:37:30 +0500

    all complete

--- src/App.js
+++ src/App.js
@@ -4,41 +4,55 @@ import './App.css';
 
 const App = () => {
   const viewer = useRef(null);
-
-  // if using a class, equivalent of componentDidMount 
   useEffect(() => {
+   
+    
     WebViewer(
       {
         path: '/webviewer/lib',
-        initialDoc: '/files/PDFTRON_about.pdf',
-        licenseKey: 'your_license_key'  // sign up to get a free trial key at https://dev.apryse.com
+        licenseKey: 'your_license_key',
+        enableMeasurement: true,
+        enableAnnotations:true,
+        enableFilePicker:true,
+      
+        
       },
       viewer.current,
     ).then((instance) => {
-      const { documentViewer, annotationManager, Annotations } = instance.Core;
-
+      console.log(instance);
+      const { documentViewer, annotationManager, Annotations} = instance.Core;
+      // document.getElementById('file-picker').onchange = e => {
+      //   const file = e.target.files[0];
+      //   if (file) {
+      //     instance.UI.loadDocument(file);
+      //     // document.getElementById('viewer').style.display='block';
+      //   }
+      // };   
+      
       documentViewer.addEventListener('documentLoaded', () => {
         const rectangleAnnot = new Annotations.RectangleAnnotation({
           PageNumber: 1,
-          // values are in page coordinates with (0, 0) in the top left
           X: 100,
           Y: 150,
           Width: 200,
           Height: 50,
           Author: annotationManager.getCurrentUser()
         });
-
         annotationManager.addAnnotation(rectangleAnnot);
-        // need to draw the annotation otherwise it won't show up until the page is refreshed
         annotationManager.redrawAnnotation(rectangleAnnot);
+      
       });
     });
   }, []);
-
   return (
     <div className="App">
-      <div className="header">React sample</div>
-      <div className="webviewer" ref={viewer}></div>
+      <div className="header">PdfEditor</div>
+      {/* <div className='app-container'>
+        <h1 className='home-h1'>Powerful online PDF tools to make your document workflows easy and secure</h1>
+        <input id="file-picker" type="file" accept=".pdf,.jpg,.jpeg,.png,.docx,.xlsx,.pptx,.md" />
+        <p>Upload documents of up to 25 MB in PDF, DOC, DOCX, RTF, PPT, PPTX, JPEG, PNG, or TXT</p>
+      </div> */}
+      <div id='viewer' className="webviewer" ref={viewer}></div>
     </div>
   );
 };
